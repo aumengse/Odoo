da105c3ec6e55559a6b15b4f360e8c007f119e50
Aurel <balanayaerole@gmail.com>
2022-03-20 11:55:23 +0800

    [EDIT] asec_customs

--- asec_customs/models/__init__.py
+++ asec_customs/models/__init__.py
@@ -7,6 +7,5 @@ from . import training_program_courses
 from . import training_program_venue
 from . import training_program_document
 from . import training_program_document_type
-from . import training_program_materials
 from . import training_program
 from . import training_program_participant
